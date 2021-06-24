69a27a6c43ca565650e3bfe98253636beb5281e8
Vasiliy <vash050@rambler.ru>
2021-06-24 12:49:34 +0300

    - anagram

--- test_task/main.py
+++ test_task/main.py
@@ -6,3 +6,20 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {"key": "Hello"}
+
+
+@app.post('/anagram/')
+def anagram(str_1: str, str_2: str):
+    new_str_1 = [x for x in str_1 if x.isalpha() or x.isdigit()]
+    new_str_2 = [x for x in str_2 if x.isalpha() or x.isdigit()]
+
+    str_1_pars = "".join(new_str_1).lower()
+    str_2_pars = "".join(new_str_2).lower()
+
+    flag = False
+    if len(str_1_pars) == len(str_2_pars):
+        for el in str_1_pars:
+            flag = el in str_2_pars
+            if flag is False:
+                break
+    return {'key': flag}
