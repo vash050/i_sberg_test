ca17d6acceaf82cabe9ae2c1540d62aced371e5d
Vasiliy <vash050@rambler.ru>
2021-06-25 19:02:19 +0300

    -done

--- test_task/functions.py
+++ test_task/functions.py
@@ -1,38 +1,71 @@
 import random
 
 
+def get_processed_strings(str_raw):
+    """
+    the function gets a string, creates a new one from it only from numbers and letters. Returns it.
+    :param str_raw: str
+    :return: str
+    """
+    return "".join(filter(lambda x: x.isalpha() or x.isdigit(), str_raw)).lower()
+
+
+def get_check(item):
+    """
+    the function gets an object from two strings, checks whether they are an anagram.
+    :param item: object model Anagram
+    :return: bool
+    """
+    str_1_pars = get_processed_strings(item.str_1)
+    str_2_pars = get_processed_strings(item.str_2)
+
+    flag = False
+    if len(str_1_pars) == len(str_2_pars):
+        for el in str_1_pars:
+            flag = el in str_2_pars
+            if flag is False:
+                break
+    return flag
+
+
 def get_mac_address():
-    '''
-    functions create random hex number(MAC address)
+    """
+    function creates random hex number(MAC address)
     :return: hex
-    '''
+    """
     x = random.randint(100000000000000, 199999999999999)
     mac_address = f'{x:x}'
-
+    ############################################################
+    # это второй вариант, более надежный, но более длинный.
     # "%02x%02x%02x%02x%02x%02x" % (random.randint(0, 255),
     #                               random.randint(0, 255),
     #                               random.randint(0, 255),
     #                               random.randint(0, 255),
     #                               random.randint(0, 255),
     #                               random.randint(0, 255))
-
+    #############################################################
     return mac_address
 
 
 def get_dev_type():
-    '''
-    function return random element from tuple
+    """
+    function returns random element from tuple
     :return: str
-    '''
+    """
     dev_types = ('emeter', 'zigbee', 'lora', 'gsm')
     idx = random.randint(0, 3)
     return dev_types[idx]
 
 
 def get_devices(count):
+    """
+    function gets number of objects, creates and returns them
+    :param count: int
+    :return: list
+    """
     list_devices = []
     for el in range(count):
-        device = {'dev_id': '', 'dev_type': ''}
+        device = {}
         device['dev_id'] = get_mac_address()
         device['dev_type'] = get_dev_type()
         list_devices.append(device)
--- test_task/main.py
+++ test_task/main.py
@@ -1,18 +1,11 @@
-from fastapi import FastAPI, Response, status
-
-import asyncio
 import aioredis
 import asyncpg
+from fastapi import FastAPI, Response, status
 
-from functions import get_devices
+from functions import get_devices, get_check
 from schemas import Anagram
 
 app = FastAPI()
-
-
-@app.get('/')
-def home():
-    return {"key": "Hello"}
 
 
 @app.post('/anagram/')
@@ -22,18 +15,7 @@ async def anagram(item: Anagram):
     :param item:
     :return:
     '''
-    new_str_1 = [x for x in item.str_1 if x.isalpha() or x.isdigit()]
-    new_str_2 = [x for x in item.str_2 if x.isalpha() or x.isdigit()]
-
-    str_1_pars = "".join(new_str_1).lower()
-    str_2_pars = "".join(new_str_2).lower()
-
-    flag = False
-    if len(str_1_pars) == len(str_2_pars):
-        for el in str_1_pars:
-            flag = el in str_2_pars
-            if flag is False:
-                break
+    flag = get_check(item)
     redis = await aioredis.create_redis_pool('redis://localhost:6379')
     count = await redis.get('count', encoding='utf-8')
     if flag:
@@ -50,41 +32,18 @@ async def anagram(item: Anagram):
     return {'key': flag, 'count': count}
 
 
-@app.get('/drop-table-device')
-async def delete_table_device():
-    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
-    await conn.execute("DROP TABLE device")
-    await conn.close()
-    return {'status': 'OK'}
-
-
-@app.get('/drop-table-endpoint')
-async def delete_table_device():
-    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
-    await conn.execute("DROP TABLE endpoint")
-    await conn.close()
-    return {'status': 'OK'}
-
-
-@app.get('/create-table-device/')
-async def create_table_devices():
+@app.get('/create-tables/')
+async def create_table_device():
     conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
     await conn.execute("CREATE TABLE device(id serial PRIMARY KEY, dev_id varchar , dev_type varchar)")
-    await conn.close()
-    return {'status': 'OK'}
-
-
-@app.get('/create-table-endpoint/')
-async def create_table_devices():
-    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
     await conn.execute(
         "CREATE TABLE endpoint(id serial PRIMARY KEY, device_id INT, FOREIGN KEY (device_id) REFERENCES device (id) ON DELETE CASCADE)")
     await conn.close()
     return {'status': 'OK'}
 
 
-@app.get('/devices-into-db/')
-async def device_into():
+@app.get('/devices-into-db/', status_code=200)
+async def device_into(response: Response):
     count_el = 10
     conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
     devices = get_devices(count_el)
@@ -93,46 +52,61 @@ async def device_into():
                             INSERT INTO device(dev_id, dev_type) VALUES($1, $2)
                             ''',
                            el['dev_id'], el['dev_type'])
-    await conn.close()
-    return {'status': '201'}
-
-
-@app.get('/devices-from-db/')
-async def get_devices_from_db():
-    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
-    rows = await conn.fetch('SELECT * FROM device')
-    await conn.close()
-    return rows
-
-
-@app.get('/devices-from-db-5-random/, status_code=200')
-async def get_devices_from_db(response: Response):
-    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
     device_id = await conn.fetch('SELECT id FROM device ORDER BY RANDOM() LIMIT 5')
     for el in device_id:
         await conn.execute('''
                             INSERT INTO endpoint(device_id) VALUES($1)
                             ''',
                            el['id'])
-    endpoint = await conn.fetch('SELECT * FROM endpoint')
     await conn.close()
     response.status_code = status.HTTP_201_CREATED
-    return endpoint
+    return {'status': '201'}
+
+
+@app.get('/get_all_device_without_endpoint/')
+async def get_all_device_without_endpoint():
+    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
+    all_devices = await conn.fetch(
+        'SELECT * FROM device WHERE id NOT IN (SELECT device_id FROM endpoint) ORDER BY dev_type')
+    await conn.close()
+    return all_devices
+
+
+@app.get('/devices-from-db/')
+async def get_all_devices_from_db():
+    """
+     function for control
+    :return:
+    """
+    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
+    rows = await conn.fetch('SELECT * FROM device')
+    await conn.close()
+    return rows
 
 
 @app.get('/get_all_endpoint/')
 async def get_all_endpoint():
+    """
+    function for control
+    :return:
+    """
     conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
     all_endpoint = await conn.fetch('SELECT * FROM endpoint')
     await conn.close()
     return all_endpoint
 
 
-@app.get('/get_all_device_without_endpoint/')
-async def get_all_device_without_endpoint():
+@app.delete('/drop-table-device')
+async def delete_table_device():
     conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
-    # all_devices = await conn.fetch('SELECT * FROM device JOIN endpoint ON device.id = endpoint.device_id ORDER BY dev_type')
-    all_devices = await conn.fetch(
-        'SELECT * FROM device WHERE id NOT IN (SELECT device_id FROM endpoint) ORDER BY dev_type')
+    await conn.execute("DROP TABLE device")
     await conn.close()
-    return all_devices
+    return {'status': 'OK'}
+
+
+@app.delete('/drop-table-endpoint')
+async def delete_table_endpoint():
+    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:8010/stage')
+    await conn.execute("DROP TABLE endpoint")
+    await conn.close()
+    return {'status': 'OK'}
